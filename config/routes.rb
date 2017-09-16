85966e6bf131dfebfe23dd9ff9a3832c07c3012c
kothariaditya <kothariaditya7@gmail.com>
2017-09-16 11:28:48 -0400

    setup for records table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'records/:id', to: 'records#read'
+
   get 'prescriptions/:id', to: 'prescriptions#read'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

adc25f43c83bb1a6cfb319bc9b3a929727b389df
kothariaditya <kothariaditya7@gmail.com>
2017-09-16 10:55:51 -0400

    adding prescription table with pg setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'prescriptions/:id', to: 'prescriptions#read'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
